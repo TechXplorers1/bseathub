12975f4a08e457f60896c8c398cfb7d1a8a09fcb
Sapare Sandeep <sandeepsapare.tx@gmail.com>
2025-10-22 05:41:21 +0000

    In the main page, I need a section that shows 8 food cards. These cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,18 @@ import { FilterCategories } from '@/components/home/FilterCategories';
 import { HomeFeed } from '@/components/home/HomeFeed';
 import { HomeFoodCarousel } from '@/components/home/HomeFoodCarousel';
 import { Personalized } from '@/components/home/Personalized';
+import { RestaurantCarousel } from '@/components/home/RestaurantCarousel';
 import { allRestaurants } from '@/lib/data';
 
 export default function Home() {
+  const whatsOnYourMindRestaurants = [...allRestaurants].sort(() => 0.5 - Math.random());
   return (
     <div className="flex flex-col">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <FilterCategories />
         <Banners />
         <Personalized />
-        <HomeFoodCarousel />
+        <RestaurantCarousel title="What's on your mind today!" restaurants={whatsOnYourMindRestaurants} />
         <HomeFeed restaurants={allRestaurants} />
         <ChefsCarousel />
       </div>

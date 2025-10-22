e3891640db47d01f7effa5d6b21a606088149623
Sapare Sandeep <sandeepsapare.tx@gmail.com>
2025-10-22 06:42:59 +0000

    remove this "What's on your mind today!" & replace with this "Home Food"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import { Banners } from '@/components/home/Banners';
 import { ChefsCarousel } from '@/components/home/ChefsCarousel';
 import { FilterCategories } from '@/components/home/FilterCategories';
@@ -5,17 +6,16 @@ import { HomeFeed } from '@/components/home/HomeFeed';
 import { HomeFoodCarousel } from '@/components/home/HomeFoodCarousel';
 import { Personalized } from '@/components/home/Personalized';
 import { RestaurantCarousel } from '@/components/home/RestaurantCarousel';
-import { allRestaurants } from '@/lib/data';
+import { allRestaurants, allHomeFoods } from '@/lib/data';
 
 export default function Home() {
-  const whatsOnYourMindRestaurants = [...allRestaurants].sort(() => 0.5 - Math.random());
   return (
     <div className="flex flex-col">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <FilterCategories />
         <Banners />
         <Personalized />
-        <RestaurantCarousel title="What's on your mind today!" restaurants={whatsOnYourMindRestaurants} />
+        <RestaurantCarousel title="Home Food" restaurants={allHomeFoods} href="/home-food" />
         <HomeFeed restaurants={allRestaurants} />
         <ChefsCarousel />
       </div>

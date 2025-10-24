b3e7706b49e7d0e6d8523c7fc31a29999c95cdd1
Sapare Sandeep <sandeepsapare.tx@gmail.com>
2025-10-24 18:30:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4431792229-3298f",
+  "appId": "1:152362653097:web:53b4491c8d1fe7389760b8",
+  "apiKey": "AIzaSyDi1ZEtiHh8iw25gajzN3qtrFiP0dKSeEs",
+  "authDomain": "studio-4431792229-3298f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "152362653097"
+};

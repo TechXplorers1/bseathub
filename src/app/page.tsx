94c2bafbd9dc4588963dca8aea6418465e0da71d
Bharath Surya <bharath2.tx@gmail.com>
2025-11-05 07:55:11 +0000

    make the home page width 100vw and adjust everything in the main home pa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,20 @@ export default function Home() {
   const { restaurants, homeFoods } = useRestaurants();
   return (
     <div className="flex flex-col">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <FilterCategories />
-        <Banners />
-        <Personalized />
-        <MostOrdered />
-        <RestaurantCarousel title="Home Food" restaurants={homeFoods} href="/home-food" />
-        <HomeFeed restaurants={restaurants} />
-        <ChefsCarousel />
+      <div className="w-full py-8">
+        <div className="px-4 sm:px-6 lg:px-8">
+          <FilterCategories />
+          <Banners />
+          <Personalized />
+          <MostOrdered />
+        </div>
+        <div className="px-4 sm:px-6 lg:px-8">
+          <RestaurantCarousel title="Home Food" restaurants={homeFoods} href="/home-food" />
+        </div>
+        <div className="px-4 sm:px-6 lg:px-8">
+          <HomeFeed restaurants={restaurants} />
+          <ChefsCarousel />
+        </div>
       </div>
     </div>
   );

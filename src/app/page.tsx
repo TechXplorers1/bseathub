e5abf29409eceb7a7a244f600b6ca156885ec066
Bharath Surya <bharath2.tx@gmail.com>
2025-11-05 12:03:26 +0000

    decrease the width in the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,20 +14,14 @@ export default function Home() {
   const { restaurants, homeFoods } = useRestaurants();
   return (
     <div className="flex flex-col">
-      <div className="w-full py-8">
-        <div className="px-4 sm:px-6 lg:px-8">
-          <FilterCategories />
-          <Banners />
-          <Personalized />
-          <MostOrdered />
-        </div>
-        <div className="px-4 sm:px-6 lg:px-8">
-          <RestaurantCarousel title="Home Food" restaurants={homeFoods} href="/home-food" />
-        </div>
-        <div className="px-4 sm:px-6 lg:px-8">
-          <HomeFeed restaurants={restaurants} />
-          <ChefsCarousel />
-        </div>
+      <div className="w-full py-8 container">
+        <FilterCategories />
+        <Banners />
+        <Personalized />
+        <MostOrdered />
+        <RestaurantCarousel title="Home Food" restaurants={homeFoods} href="/home-food" />
+        <HomeFeed restaurants={restaurants} />
+        <ChefsCarousel />
       </div>
     </div>
   );

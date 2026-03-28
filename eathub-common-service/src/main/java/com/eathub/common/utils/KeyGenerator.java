8bbba5dc1d24af59950fb0a6c279d858c918f084
chaveen-tx7 <bharath@techxplorers.in>
2026-03-28 10:20:10 +0530

    need updates

--- eathub-common-service/src/main/java/com/eathub/common/utils/KeyGenerator.java
+++ eathub-common-service/src/main/java/com/eathub/common/utils/KeyGenerator.java
@@ -1,9 +1,12 @@
+package com.eathub.common.utils;
+
 import io.jsonwebtoken.security.Keys;
 import java.util.Base64;
 
 public class KeyGenerator {
     public static void main(String[] args) {
-        String secret = Base64.getEncoder().encodeToString(Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256).getEncoded());
+        String secret = Base64.getEncoder()
+                .encodeToString(Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256).getEncoded());
         System.out.println(secret);
     }
 }
